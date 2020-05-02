9f68a48833b7543dfa606bf467e0edbf143979b0
JBosch <javier.bosch@outlook.com>
2020-05-02 20:29:18 +0100

    forces

--- body.py
+++ body.py
@@ -1,18 +1,46 @@
 import pygame
+import math
+velocity_constant = 50
+
 class Body:
+
 	def __init__(self, position, radius, screen):
 		self.position = position
 		self.radius = radius
+		self.mass = (4/3) * math.pi * (self.radius**3)
 		self.screen = screen
 		self.done = False
 		self.color = (255,255,255)
+		self.forces = (0.0,0.0)
 
 	def draw(self):
 		pygame.draw.circle(self.screen, self.color , self.position, self.radius)
 
 	def enlarge(self):
 		self.radius += 1
+		self.mass = (4/3) * math.pi * (self.radius**3)
 
-	def finish(self):
+	def finish(self, mouse_position):
 		self.color = (255,0,0)
+		self.velocity = tuple(map(lambda i, j: (i - j)//velocity_constant, self.position, mouse_position)) 
+		self.mass = (4/3) * math.pi * (self.radius**3)
+		self.done = True
+	
+	def move(self):
+		self.position = tuple(map(lambda i, j: int (i + j), self.position, self.velocity)) 
+		a = tuple(map(lambda i: i/self.mass, self.forces)) 
+		self.velocity = tuple(map(lambda i, j: i + j, self.velocity, a))
+		self.forces = (0.0,0.0)
+
+	def collision(self,other):
+		distance = math.sqrt( ((self.position[0] - other.position[0])**2) + ((self.position[1] - other.position[1])**2) )
+		total_radiuus = self.radius + other.radius
+		return distance<total_radiuus
+
+	def collided_body(self,other,another):
+		self.color = (0,255,0)
+		self.mass = other.mass + another.mass
+		self.position = tuple(map(lambda i, j: int ((i*other.mass + j*another.mass)/ self.mass) , other.position, another.position))
+		self.radius =  (int(round((((3/4) * self.mass)/math.pi)  ** (1. / 3))))
+		self.velocity = (0.0,0.0)
 		self.done = True
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import sys, pygame
+import itertools
 from body import Body
 
 pygame.init()
 
-size = width, height = 920, 680
+base_ball_size = 1
+size = width, height = 1220, 880
 speed = [2, 2]
 black = 0, 0, 0
+G = 5 * (10**(-3))
 
 screen = pygame.display.set_mode(size)
 balls = []
@@ -13,6 +16,31 @@ balls = []
 print ("Hi")
 
 last_ball_done = True
+
+def add_forces(body1,body2):
+        dx = body2.position[0] - body1.position[0]
+        dy = body2.position[1] - body1.position[1]
+        r_squared = dx*dx + dy*dy
+        r = r_squared**0.5
+        force_magnitude = (G * body1.mass * body2.mass) / r_squared #F=G*M1*M2/(r^2)
+        dx_normalized_scaled = (dx / r) * force_magnitude
+        dy_normalized_scaled = (dy / r) * force_magnitude
+        body1.forces = (body1.forces[0] + dx_normalized_scaled, body1.forces[1] + dy_normalized_scaled)
+        body2.forces = (body2.forces[0] - dx_normalized_scaled, body2.forces[1] - dy_normalized_scaled)
+
+def collisions(bodies):
+    list_to_check = list(itertools.combinations(bodies, 2))
+    for pair in list_to_check:
+
+        add_forces(pair[0],pair[1])
+
+        if (pair[0].collision(pair[1])):
+            ball = Body( pygame.mouse.get_pos(),base_ball_size,screen )
+            ball.collided_body(pair[0],pair[1])
+            bodies.append(ball)
+            bodies.remove(pair[0])
+            bodies.remove(pair[1])
+            
 
 while 1:
     pygame.time.delay(20)
@@ -22,7 +50,7 @@ while 1:
 
 
     if(pygame.mouse.get_pressed()[0] and last_ball_done): 
-        ball = Body( pygame.mouse.get_pos(),5,screen )
+        ball = Body( pygame.mouse.get_pos(),base_ball_size,screen )
         balls.append(ball)
         last_ball_done = False
     
@@ -30,13 +58,16 @@ while 1:
         ball.enlarge()
 
     if(not (pygame.mouse.get_pressed()[0] or last_ball_done)): 
-        ball.finish()
+        ball.finish(pygame.mouse.get_pos())
         last_ball_done = True
 
 
     screen.fill(black)
-    
+    collisions(balls)
     for ball in balls:
+        print(ball.forces)
+        if (ball.done):
+            ball.move()
         ball.draw()
 
     pygame.display.flip()
